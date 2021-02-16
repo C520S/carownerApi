8534d03f93cf260c2937f4b857d9e42cb673ab7a
Frank_glb <2179869390@qq.com>
2021-02-16 16:38:09 +0200

    add carowner

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/book');
 const carRouter = require('./routes/car');
+const carownerRouter = require('./routes/carowner');
 
 const cors = require('cors');
 const helmet = require('helmet');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/book', booksRouter);
 app.use('/car', carRouter);
+app.use('/carowner', carownerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
